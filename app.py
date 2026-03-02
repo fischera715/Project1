f155dcba9c89da02dd2c9845b10ee7311f15b11e
fischera715 <fischera@sunypoly.edu>
2026-03-02 11:41:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,8 @@ df = pd.read_csv("events-US-1980-2024.csv", skiprows=2)
 
 df.columns = df.columns.str.strip()
 
-if st.checkbox("Show Raw Data"):
+if st.checkbox("Show Data"):
     st.dataframe(df)
-
-st.write("Dataset loaded successfully.")
 
 df['Begin Date'] = pd.to_datetime(df['Begin Date'], format='%Y%m%d')
 df['End Date'] = pd.to_datetime(df['End Date'], format='%Y%m%d')
@@ -23,3 +21,21 @@ df['Year'] = df['Begin Date'].dt.year
 
 df['Duration'] = (df['End Date'] - df['Begin Date']).dt.days
 df["CPI-Adjusted Cost"] = pd.to_numeric(df["CPI-Adjusted Cost"], errors="coerce")
+
+st.header("Trend in Number of Billion-Dollar Disasters")
+
+disasters_per_year = df.groupby("Year").size().reset_index(name="Count")
+
+fig1 = px.bar(
+    disasters_per_year,
+    x="Year",
+    y="Count",
+    title="Number of Disasters per Year",
+)
+
+st.plotly_chart(fig1, use_container_width=True)
+
+st.write(
+    "This chart shows the annual frequency of billion-dollar disasters. "
+    "Notice the increase in event counts in more recent decades."
+)
